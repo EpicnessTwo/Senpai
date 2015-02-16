dfb65c380704985cff883f284fe3b2da0ea5298a
EpicKitty <epicnesstwo@epicnesstwo.tk>
2015-02-16 22:15:15 +0000

    Added a git pull function to download the latest source code

--- Phergie/Plugin/Console.php
+++ Phergie/Plugin/Console.php
@@ -70,4 +70,33 @@ class Phergie_Plugin_Console extends Phergie_Plugin_Abstract
     	    $this->plugins->send->send($source, $this->getConfig('error.noperms') , $nick);
     	}
     }
+    
+    public function onCommandUpdate()
+    {
+        $event = $this->getEvent();
+		$source = $event->getSource();
+		$nick = $event->getNick();
+		$hostmask = explode("!", $this->event->getHostmask());
+		$hostmask = $hostmask[1];
+		
+		if ($this->plugins->permission->getLevel($hostmask) >= 3)
+    	{
+			$data = preg_split('/[\r\n]+/', exec("git pull"), -1, PREG_SPLIT_NO_EMPTY);
+		    $counter = 0;
+		    while($counter !== count($data))
+				{   
+				    if($data[$counter] == "Already up-to-date.")
+				    {
+					    $this->plugins->send->send($source, "The source code is already at the latest version. No need to update!", $nick);
+					    $counter++;
+				    } else {
+				    $this->plugins->send->send($source, "Updating source code and restarting the bot!", $nick);
+				    $this->doQuit("Source updated - Restart required!");
+				    } 
+				    
+				}
+    	} else {
+    	    $this->plugins->send->send($source, $this->getConfig('error.noperms') , $nick);
+    	}
+    }
 }
