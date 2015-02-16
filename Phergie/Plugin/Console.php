34487b465a404976ddc1fe04518a7427d039774d
EpicKitty <epicnesstwo@epicnesstwo.tk>
2015-02-16 20:40:52 +0000

    Added a git pushing command

--- Phergie/Plugin/Console.php
+++ Phergie/Plugin/Console.php
@@ -51,4 +51,23 @@ class Phergie_Plugin_Console extends Phergie_Plugin_Abstract
     	    $this->plugins->send->send($source, $this->getConfig('error.noperms') , $nick);
     	}
     }
+    
+    public function onCommandPush($args)
+    {
+        $event = $this->getEvent();
+		$source = $event->getSource();
+		$nick = $event->getNick();
+		$hostmask = explode("!", $this->event->getHostmask());
+		$hostmask = $hostmask[1];
+		
+		if ($this->plugins->permission->getLevel($hostmask) >= 3)
+    	{
+			exec("git add *");
+			exec("git commit -m '$args'");
+			exec("git push");
+			$this->plugins->send->send($source, "Pushed to the master repo!", $nick);
+    	} else {
+    	    $this->plugins->send->send($source, $this->getConfig('error.noperms') , $nick);
+    	}
+    }
 }
