af8f65ad71f4fbb0afc1e9a1c6ea6ee3cf3c8cdf
EpicKitty <epicnesstwo@epicnesstwo.tk>
2015-06-20 08:35:01 +0000

    Working on the new ping6 command

--- Phergie/Plugin/Utilities.php
+++ Phergie/Plugin/Utilities.php
@@ -241,15 +241,6 @@ class Phergie_Plugin_Utilities extends Phergie_Plugin_Abstract
             $args = str_ireplace(array("http://", "https://"), array("", ""), $args);
         
         
-        	// IPv6 Handling
-        	// This detects if the input args is an IPv6 address and runs if
-        	// it is.
-        if (filter_var($args , FILTER_VALIDATE_IP, FILTER_FLAG_IPV6))
-        {
-            //IPv6
-            $this->plugins->send->send($source, "IPv6 Not Implemented Yet", $nick);
-        } else 
-        
         	// IPv4 Handling
         	// This detects if the input args is an IPv4 address and runs if
         	// it is.
@@ -323,9 +314,123 @@ class Phergie_Plugin_Utilities extends Phergie_Plugin_Abstract
                 // for the host command in their arguments.
             if ($this->getConfig('commands.fancyOutput'))
             {
-                $this->plugins->send->send($source, "\x02\x0304This is not a valid IPv4, IPv6 or Hostname", $nick);
+                $this->plugins->send->send($source, "\x02\x0304This is not a valid IPv4 or Hostname", $nick);
             } else {
-                $this->plugins->send->send($source, "This is not a valid IPv4, IPv6 or Hostname", $nick);
+                $this->plugins->send->send($source, "This is not a valid IPv4 or Hostname", $nick);
+            }
+        } 
+    }
+    
+        /*
+     * This is the ping command and it's still WIP (Work in progress)!
+     * Please use this command wisely as it may still contain bugs or exploits that I
+     * am yet to find and patch.
+     *
+     * P.S. I need to write more about this command once I've finished it!
+     */
+    public function onCommandPing6($args)
+    {
+        $event = $this->getEvent();
+		$source = $event->getSource();
+		$nick = $event->getNick();
+		$hostmask = explode("!", $this->event->getHostmask());
+		$hostmask = $hostmask[1];
+		
+		// Variables
+		
+		    // Setting the following lines to true will disable them. This is in case
+		    // the server that is running the bot does not support IPv4 or IPv6.
+		    // Error handling may not be implemented in further down so do make
+		    // sure this is configured correctly!
+		$noIPv4 = false;
+		$noIPv6 = false;
+		
+		    // How many times do you want the ping command to cycle? (Default: 4)
+		$pingCount = 4;
+		
+		    // Pre-Fun things
+            // This removes the https:// and http:// from the front of the $args
+            // just incase the user left them there
+            
+            $args = str_ireplace(array("http://", "https://"), array("", ""), $args);
+        
+        
+        	// IPv4 Handling
+        	// This detects if the input args is an IPv4 address and runs if
+        	// it is.
+        if (filter_var($args , FILTER_VALIDATE_IP, FILTER_FLAG_IPV6))
+        {
+            //IPv6
+            
+                // Executes the "ping" command and stores the data in the array "$data"
+                // The data is split at newlines
+            $data = shell_exec("ping6 -c $pingCount -v -q " . escapeshellcmd($args));
+	    	$data = preg_split('/[\r\n]+/', $data, -1, PREG_SPLIT_NO_EMPTY);
+	    	
+	    	    // Grabs the IP Address from the first line
+	    	$ip = explode(" ", $data[0]);
+	    	$ip = $ip[2];
+	    	
+	    	    // Grabs the total time taken from the 3rd line
+	    	$total = explode(" ", $data[2]);
+	    	$total_count = count($total) - 1;
+	    	$total = str_ireplace("ms", "", $total[$total_count]);
+	    	
+	    	    // Grabs the average from the final line
+	    	$average = explode(" ", $data[3]);
+	    	$average = explode("/", $average[3]);
+	    	$average = $average[1];
+	    	
+	    	if ($this->getConfig('commands.fancyOutput'))
+            {
+                $this->plugins->send->send($source, "\x02\x0304Pinged \x0305$args $ip \x0304a total of \x0305$pingCount \x0304times. Total time: \x0305$total \x0304ms. Average: \x0305$average \x0304ms.", $nick);
+            } else {
+                $this->plugins->send->send($source, "Pinged $args $ip a total of $pingCount times. Total time: $total ms. Average: $average ms.", $nick);
+            }
+            
+        } else
+        
+        	// Hostmask Handling
+        	// This detects if the input args is a hostmask and runs if
+        	// it is.
+        if (preg_match("/^([a-z\d](-*[a-z\d])*)(\.([a-z\d](-*[a-z\d])*))*$/i", $args) && preg_match("/^.{1,253}$/", $args) && preg_match("/^[^\.]{1,63}(\.[^\.]{1,63})*$/", $args))
+        {
+            //Hostmask
+                // Executes the "ping" command and stores the data in the array "$data"
+                // The data is split at newlines
+            $data = shell_exec("ping6 -c $pingCount -v -q " . escapeshellcmd($args));
+	    	$data = preg_split('/[\r\n]+/', $data, -1, PREG_SPLIT_NO_EMPTY);
+	    	
+	    	    // Grabs the IP Address from the first line
+	    	$ip = explode(" ", $data[0]);
+	    	$ip = $ip[2];
+	    	
+	    	    // Grabs the total time taken from the 3rd line
+	    	$total = explode(" ", $data[2]);
+	    	$total_count = count($total) - 1;
+	    	$total = str_ireplace("ms", "", $total[$total_count]);
+	    	
+	    	    // Grabs the average from the final line
+	    	$average = explode(" ", $data[3]);
+	    	$average = explode("/", $average[3]);
+	    	$average = $average[1];
+	    	
+	    	if ($this->getConfig('commands.fancyOutput'))
+            {
+                $this->plugins->send->send($source, "\x02\x0304Pinged \x0305$args $ip \x0304a total of \x0305$pingCount \x0304times. Total time: \x0305$total \x0304ms. Average: \x0305$average \x0304ms.", $nick);
+            } else {
+                $this->plugins->send->send($source, "Pinged $args $ip a total of $pingCount times. Total time: $total ms. Average: $average ms.", $nick);
+            }
+            
+        } else {
+                // It will only get here if it fails the IPv4, IPv6 and Hostname checks. This is just some
+                // extra security and also doubles up as a way of stopping people including variables for
+                // for the host command in their arguments.
+            if ($this->getConfig('commands.fancyOutput'))
+            {
+                $this->plugins->send->send($source, "\x02\x0304This is not a valid IPv6 or Hostname", $nick);
+            } else {
+                $this->plugins->send->send($source, "This is not a valid IPv6 or Hostname", $nick);
             }
         } 
     }
